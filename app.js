25207c8578310b8937dfebbf7050b666e3bea3a4
Saqib Khan <saaqibkhann8@gmail.com>
2025-10-15 15:24:38 +0500

    Completed!

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-function bgChanger(){
+function bgChanger() {
 
-
-    const colorList = colorNameList[Math.floor(Math.random()*colorNameList.length)];
+    const colorList = colorNameList[Math.floor(Math.random() * colorNameList.length)];
     console.log(colorList);
-    
+
     document.body.style.backgroundColor = colorList.hex;
-    
+    const rgb = document.body.style.backgroundColor;
     const h3 = document.getElementsByTagName("h3");
+
     h3[0].textContent = "Color Name: " + colorList.name;
     h3[1].textContent = colorList.hex;
-     const rgb = document.body.style.backgroundColor;
-     h3[2].textContent = rgb;
-     console.log(rgb);
+    h3[2].textContent = rgb;
+    console.log(rgb);
+
 };
 
 bgChanger();
